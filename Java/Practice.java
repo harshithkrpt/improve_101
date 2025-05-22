41b4fb9790ebe7a6701cb3db89435d7fe89cf195
harshith <harshith.krpt@gmail.com>
2025-05-22 21:01:05 +0530

    D

---
+++ Java/Practice.java
@@ -0,0 +1,22 @@
+import java.lang.Math;
+
+public class Practice {
+    public static void main(String args[]) {
+        int num1 = 8;
+        int num2 = 20;
+       
+        int ii[] = {1,2,3};
+
+        int result = num1 + num2;
+        System.out.println(result);
+        System.out.print(ii);
+
+        double numarray[] = new double[4];
+        System.err.println(numarray);
+
+        for(int i=0;i<4;i++) {
+            numarray[i] = Math.random();
+            System.out.println(i + " " + numarray[i]);
+        }
+    }
+}
--- Java/Practive.java
+++
@@ -1,9 +0,0 @@
-public class Practice {
-    public static void main(String args[]) {
-        int num1 = 8;
-        int num2 = 20;
-
-        int result = num1 + num2;
-        System.out.println(result);
-    }
-}
