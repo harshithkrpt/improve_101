b3dcbcd9df5ead4d3b216e2388cb6a2cc7116db1
harshith <harshith.krpt@gmail.com>
2025-05-24 11:00:03 +0530

    Java Practice

--- Java/Practice.java
+++ Java/Practice.java
@@ -18,5 +18,30 @@ public class Practice {
             numarray[i] = Math.random();
             System.out.println(i + " " + numarray[i]);
         }
+
+
+        // Jagged Array
+        int ar[][] = new int[3][];
+        ar[0] = new int[10];
+        ar[1] = new int[12];
+        ar[2] = new int[2];
+        for(int i=0;i< ar.length;i++) {
+            for(int j=0;j<ar[i].length;j++) {
+                ar[i][j] = (int) (Math.random() * 1000);
+            }
+        }
+
+          for(int i=0;i< ar.length;i++) {
+            for(int j=0;j<ar[i].length;j++) {
+                System.out.print(ar[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        for(int[] arr: ar) {
+            for(int a: arr) {
+                System.out.print(a + "  ");
+            }
+        }
     }
 }
