7f68d15a73363597aafc38c8d07939965c05b6f7
harshith <harshith.krpt@gmail.com>
2025-06-04 20:58:50 +0530

    Added Password Config and Security Config

--- LEARNING_101/Project_For_Ultimate_Learning/SERVICES/EcommerceServer/TASKS.md
+++ LEARNING_101/Project_For_Ultimate_Learning/SERVICES/EcommerceServer/TASKS.md
@@ -8,3 +8,11 @@
 - JoinColumn annotation what is it 
 
 - Many To Many Example We Need
+
+- Add Validation simple and robost without reusability
+- how to and when to use Dto
+- how to handle exceptions in service layer
+
+- Learn more about streams + collections library
+- Collectors , :: Syntax in streams
+- ? in Generics learn next topics in generics
---
+++ LEARNING_101/Project_For_Ultimate_Learning/SERVICES/EcommerceServer/src/main/java/com/harshith/EcommerceServer/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.harshith.EcommerceServer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.harshith.EcommerceServer.security.CustomUserDetailsService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final CustomUserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(CustomUserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(this.userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth.requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated())
+            .httpBasic(Customizer.withDefaults());
+        http.authenticationProvider(this.authenticationProvider());
+        return http.build();
+    }
+}
