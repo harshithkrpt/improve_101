cce3a1e0ac66f36b3008f3d8e9e73eb0177e41ab
harshith <harshith.krpt@gmail.com>
2025-06-10 07:46:25 +0530

    Fixed liquibase issue

--- LEARNING_101/Project_For_Ultimate_Learning/SERVICES/EcommerceServer/src/main/java/com/harshith/EcommerceServer/config/LiquibaseConfig.java
+++ LEARNING_101/Project_For_Ultimate_Learning/SERVICES/EcommerceServer/src/main/java/com/harshith/EcommerceServer/config/LiquibaseConfig.java
@@ -2,19 +2,32 @@ package com.harshith.EcommerceServer.config;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import liquibase.exception.LiquibaseException;
 import liquibase.integration.spring.SpringLiquibase;
 
 @Configuration
 public class LiquibaseConfig {
+    private static final Logger logger = LoggerFactory.getLogger(LiquibaseConfig.class);
 
+    // LiquibaseConfig.java
     @Bean
     public SpringLiquibase liquibase(DataSource dataSource) {
-        SpringLiquibase lb = new SpringLiquibase();
+        SpringLiquibase lb = new SpringLiquibase() {
+            @Override
+            public void afterPropertiesSet() throws LiquibaseException {
+                logger.info("Starting Liquibase with changelog={}", getChangeLog());
+                super.afterPropertiesSet();
+                logger.info("Liquibase ran successfully");
+            }
+        };
         lb.setDataSource(dataSource);
-        lb.setChangeLog("classpath:/db/changelog/db.changelog-master.yaml");
+        // <-- remove "classpath:/"
+        lb.setChangeLog("db/changelog/db.changelog-master.yaml");
         lb.setContexts("development,production");
         lb.setDefaultSchema("ecommercespring");
         lb.setShouldRun(true);
