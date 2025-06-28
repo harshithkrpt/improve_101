23bee277b2a2f917c486c869ba6d44fb3bfc13ca
Harshith Kurapati <harshith.krpt@gmail.com>
2025-06-28 17:43:51 +0530

    basic trie data structure

---
+++ dsa/neetcode/python/trie/add_search_word.py
@@ -0,0 +1,34 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEndOfWord = False
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for ch in word:
+            if ch not in cur.children:
+                cur.children[ch] = TrieNode()
+            cur = cur.children[ch]
+        cur.isEndOfWord = True
+
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            cur = root
+            for i in range(j, len(word)):
+                ch = word[i]
+                if ch == '.':
+                    for child in cur.children.values():
+                        if dfs(i+1, child):
+                            return True
+                    return False
+                else:
+                    if ch not in cur.children:
+                        return False
+                    cur = cur.children[ch]
+            return cur.isEndOfWord
+        return dfs(0, self.root)
+        
