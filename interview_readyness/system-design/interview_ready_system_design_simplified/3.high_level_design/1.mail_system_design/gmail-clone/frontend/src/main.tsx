84d9b6340684422fa4c0880cdb7489d72ee00d20
harshith <harshith.krpt@gmail.com>
2025-07-20 20:56:40 +0530

    fixed the styling issue

--- interview_readyness/system-design/interview_ready_system_design_simplified/3.high_level_design/1.mail_system_design/gmail-clone/frontend/src/main.tsx
+++ interview_readyness/system-design/interview_ready_system_design_simplified/3.high_level_design/1.mail_system_design/gmail-clone/frontend/src/main.tsx
@@ -6,9 +6,5 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <div className="p-6 bg-blue-500 text-white text-xl rounded shadow">
-  🔥 If you see a blue box, Tailwind is back!
-</div>
-
   </React.StrictMode>
 );
